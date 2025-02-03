7ea291680ac0519045a1e41dde1f5286c6607e2a
Lucas Tenani <lucas.tenani3@gmail.com>
2025-02-03 16:45:24 -0300

    feat - Create sidebar component

--- src/pages/_layouts/app/index.tsx
+++ src/pages/_layouts/app/index.tsx
@@ -1,12 +1,15 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
   return (
-    <>
-      <header></header>
-      <main>
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="min-h-[calc(100vh-57px-97px)] flex-1 px-10 py-6 w-90 flex w-full flex-col ">
+        <SidebarTrigger />
         <Outlet />
       </main>
-    </>
+    </SidebarProvider>
   );
 }
